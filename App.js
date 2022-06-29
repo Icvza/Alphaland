3a8c459ab0f87530e4b32b0ee8f740cacf3c1aa5
Isaac <devisaacalejandro@gmail.com>
2022-06-28 20:52:06 -0400

    Added ASYNC and AppLoading

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import SwitchNavigator from './Navigation/SwitchNavigator';
 import AuthContextProvider from "./store/auth-context";
+import {useState, useEffect, useContext } from 'react'
+import AuthContextProvider from './store/auth-context';
+import { AsyncStorage } from 'react-native';
 
+function Root () {
+  const [isTryingLogin, setIstryingLogin] = useState(true)
+  const authContext = useContext(AuthContextProvider)
 
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token')
+      if (storedToken) {
+        authContext.
+      }
+    }
+  }, [])
+
+}
 
 
 export default function App() {
